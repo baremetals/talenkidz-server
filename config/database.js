c0035433aee8303124f789e91ea2651c704f65d5
Daniel Asante <baremetals16@gmail.com>
2022-05-01 15:22:19 +0100

    config changes

--- config/database.js
+++ config/database.js
@@ -2,12 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
+      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
       database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
+      user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
     },
   },
 });

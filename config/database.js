8c931f133412c7999032aa428488118ba786b7d7
Daniel Asante <baremetals16@gmail.com>
2022-10-06 13:14:15 +0100

    small change

--- config/database.js
+++ config/database.js
@@ -11,3 +11,18 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+//Cloud database settings - local development
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("CLOUD_DATABASE_HOST"),
+//       port: env.int("CLOUD_DATABASE_PORT"),
+//       database: env("CLOUD_DATABASE_NAME"),
+//       user: env("CLOUD_DATABASE_USERNAME"),
+//       password: env("CLOUD_DATABASE_PASSWORD"),
+//       ssl: env.bool("DATABASE_SSL"),
+//     },
+//   },
+// });
